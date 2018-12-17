fb825314242b1239d2051f05e377909dbfbc8405
grch0416 <xgrisha@mail.ru>
2018-12-17 13:28:05 +0400

    H2 + JDBC Console Application has been added

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO cats (name) VALUES ('Cleo');
-INSERT INTO cats (name) VALUES ('Luna');
-INSERT INTO cats (name) VALUES ('Pirat');
--- src/main/resources/schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE cats (
-        id                       serial PRIMARY KEY,
-        name                     varchar(100)
-);
