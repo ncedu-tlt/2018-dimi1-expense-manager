9a6c09a59b09d281b5d3ce7e5d7ef87a36acec4c
Polina Orlova <44366653+torichay@users.noreply.github.com>
2019-04-15 14:04:44 +0400

    Update HelloControllerTest.java

--- src/test/java/com/netcracker/ncedu/tlt/dimi1/expensemanager/HelloControllerTest.java
+++ src/test/java/com/netcracker/ncedu/tlt/dimi1/expensemanager/HelloControllerTest.java
@@ -5,11 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,18 +17,7 @@ public class HelloControllerTest {
 
     @Autowired
     private HelloController helloController;
-    /*private MockMvc mockMvc;
-
-    @LocalServerPort
-    private int port;*/
-
-    /*@Test
-    public void hello() throws Exception {
-
-        this.mockMvc.perform(get("http://localhost:" + port + "/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")));
-    }*/
-
+   
     @Test
     public void helloController(){
         assertThat(helloController).isNotNull();
